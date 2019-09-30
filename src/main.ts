1e9ad9a291d51531a6e2b098d7d078f749aaa4d8
Jesse Chen <0t2jesse@gmail.com>
2019-09-30 11:20:46 +0800

    Add simple typed.js animation

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
 import "../css/main.scss";
+import Typed, { TypedOptions } from "typed.js";
+
+const options: TypedOptions = {
+  stringsElement: "#typed-strings",
+  typeSpeed: 50
+};
+
+const typed = new Typed("#typed", options);
